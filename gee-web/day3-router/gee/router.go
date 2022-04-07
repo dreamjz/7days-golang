b8209cb3455b4abd707b7e1b2bfaad40759abb27
kesa <2356216441@qq.com>
2022-04-07 19:03:52 +0800

    Gee-Web Day3 add comment

--- gee-web/day3-router/gee/router.go
+++ gee-web/day3-router/gee/router.go
@@ -5,9 +5,10 @@ import (
 	"strings"
 )
 
+// 路由结构
 type router struct {
-	roots    map[string]*node
-	handlers map[string]HandlerFunc
+	roots    map[string]*node       // HTTP 方法对应 Trie 的根节点
+	handlers map[string]HandlerFunc // 路由对应的处理函数
 }
 
 func newRouter() *router {
@@ -25,6 +26,7 @@ func parsePattern(pattern string) []string {
 	for _, item := range vs {
 		if item != "" {
 			parts = append(parts, item)
+			// 遇到通配则退出
 			if item[0] == '*' {
 				break
 			}
@@ -33,20 +35,26 @@ func parsePattern(pattern string) []string {
 	return parts
 }
 
+// 新增路由
 func (r *router) addRoute(method string, pattern string, handler HandlerFunc) {
 	parts := parsePattern(pattern)
 
 	key := method + "-" + pattern
 	_, ok := r.roots[method]
 	if !ok {
+		// 没有根节点则创建
+		// 根节点是空的
 		r.roots[method] = &node{}
 	}
+	// 插入新节点
 	r.roots[method].insert(pattern, parts, 0)
 	r.handlers[key] = handler
 }
 
+// 获取路由
 func (r *router) getRoute(method string, path string) (*node, map[string]string) {
 	searchParts := parsePattern(path)
+	// 动态路由中的参数
 	params := make(map[string]string)
 	root, ok := r.roots[method]
 
@@ -60,9 +68,13 @@ func (r *router) getRoute(method string, path string) (*node, map[string]string)
 		parts := parsePattern(n.pattern)
 		for index, part := range parts {
 			if part[0] == ':' {
+				// 动态路由
+				// 参数名 ：参数值
 				params[part[1:]] = searchParts[index]
 			}
 			if part[0] == '*' && len(part) > 1 {
+				// 通配路由
+				//
 				params[part[1:]] = strings.Join(searchParts[index:], "/")
 				break
 			}
@@ -73,6 +85,7 @@ func (r *router) getRoute(method string, path string) (*node, map[string]string)
 	return nil, nil
 }
 
+// 获取所有路由节点
 func (r *router) getRoutes(method string) []*node {
 	root, ok := r.roots[method]
 	if !ok {
@@ -83,6 +96,7 @@ func (r *router) getRoutes(method string) []*node {
 	return nodes
 }
 
+// 处理 HTTP 请求
 func (r *router) handle(c *Context) {
 	n, params := r.getRoute(c.Method, c.Path)
 	if n != nil {
--- gee-web/day3-router/gee/trie.go
+++ gee-web/day3-router/gee/trie.go
@@ -5,34 +5,48 @@ import (
 	"strings"
 )
 
+// Trie 节点
 type node struct {
-	pattern  string
-	part     string
-	children []*node
-	isWild   bool
+	pattern  string  // 待匹配路由
+	part     string  // 当前节点对应内容
+	children []*node // 子节点
+	isWild   bool    // 通配标志
 }
 
 func (n *node) String() string {
 	return fmt.Sprintf("node{pattern=%s, part=%s, isWild=%t}", n.pattern, n.part, n.isWild)
 }
 
+// 插入节点
 func (n *node) insert(pattern string, parts []string, height int) {
+	// 递归出口
+	// 当高度和 parts 长度相同
 	if len(parts) == height {
+		// 将待匹配路由存在节点中
 		n.pattern = pattern
 		return
 	}
 
+	// 获取当前高度对应的部分
+	// 由上至下地存储 part
 	part := parts[height]
+	// 查找第一个匹配的子节点
 	child := n.matchChild(part)
+	// 没有则新增
 	if child == nil {
 		child = &node{part: part, isWild: part[0] == ':' || part[0] == '*'}
 		n.children = append(n.children, child)
 	}
+	// 插入下一层节点
 	child.insert(pattern, parts, height+1)
 }
 
+// 查找节点
 func (n *node) search(parts []string, height int) *node {
+	// 递归出口
+	// 高度和切片长度相同 或 遇到通配节点
 	if len(parts) == height || strings.HasPrefix(n.part, "*") {
+		// 节点中的 pattern 为空则表示未找到
 		if n.pattern == "" {
 			return nil
 		}
@@ -40,9 +54,11 @@ func (n *node) search(parts []string, height int) *node {
 	}
 
 	part := parts[height]
+	// 获取匹配的子节点
 	children := n.matchChildren(part)
 
 	for _, child := range children {
+		// 在子节点中递归查找
 		result := child.search(parts, height+1)
 		if result != nil {
 			return result
@@ -52,6 +68,7 @@ func (n *node) search(parts []string, height int) *node {
 	return nil
 }
 
+// 遍历所有节点，获取 pattern 不为空的节点
 func (n *node) travel(list *([]*node)) {
 	if n.pattern != "" {
 		*list = append(*list, n)
@@ -61,6 +78,7 @@ func (n *node) travel(list *([]*node)) {
 	}
 }
 
+// 寻找第一个匹配的节点
 func (n *node) matchChild(part string) *node {
 	for _, child := range n.children {
 		if child.part == part || child.isWild {
@@ -70,6 +88,7 @@ func (n *node) matchChild(part string) *node {
 	return nil
 }
 
+// 获取所有匹配节点
 func (n *node) matchChildren(part string) []*node {
 	nodes := make([]*node, 0)
 	for _, child := range n.children {
